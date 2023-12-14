b20a6ba74a8c90ce2bac6470ce994aaf13a3e217
NiklasRestory <niklas.cullberg@restory.ai>
2023-12-14 10:15:30 +0100

    Made some DesktopBranch changes.

---
+++ src/main/java/org/example/RepetitionInterfaces/DesktopEngine.java
@@ -0,0 +1,23 @@
+package org.example.RepetitionInterfaces;
+
+public class DesktopEngine implements IEngine{
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void accelerate() {
+
+    }
+
+    @Override
+    public void brake() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
