eb656f62b387721a4249c9c768b626b41a8151e6
NiklasRestory <niklas.cullberg@restory.ai>
2023-12-14 10:48:41 +0100

    Added guyiuh

---
+++ src/main/java/org/example/guyiuh.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+
+public class guyiuh {
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JTextArea textArea1;
+}
